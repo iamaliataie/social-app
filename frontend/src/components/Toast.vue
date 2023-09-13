e87b5d46d0daa8c4091d010d23ee8ee82414f32f
Ali Ahmad Ataie <iamaliataie@gmail.com>
2023-09-13 18:16:12 +0430

    Update Toast.vue

--- frontend/src/components/Toast.vue
+++ frontend/src/components/Toast.vue
@@ -1,10 +1,9 @@
 <script setup>
 
 import { useToastStore } from '../stores/toast'
-
 const toastStore = useToastStore();
-
 </script>
+
 <template>
     <div 
     class="fixed right-6 top-full text-white p-3 rounded-md transition duration-500 ease-in-out"
